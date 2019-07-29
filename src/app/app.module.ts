0e66a8e7e935528fe45fac1daf88caf31e46e1a8
rspaimoc2 <raduspaimoc@gmail.com>
2019-07-29 18:13:23 +0200

    Material interface elements added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatTableModule, MatFormFieldModule, MatInputModule  } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreatComponent } from './posts/post-create/post-create.component';
@@ -12,7 +14,9 @@ import { PostCreatComponent } from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatCardModule, MatTableModule, MatFormFieldModule , MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
