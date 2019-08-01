fe0743180f64815624acfb37ece2a3e1920b5e6f
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-01 11:34:12 +0200

    Posts fetched from db

--- backend/app.js
+++ backend/app.js
@@ -41,22 +41,14 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: 'aaaaaaa',
-      title: 'First server side post',
-      content: 'This is coming from the server,'
-    },
-    { id: 'abaaaaa',
-      title: 'Second server side post',
-      content: 'This is coming from the server,'
-    }
-  ];
-
-  res.status(200).json({
-    message: 'Posts fetched succesfully!',
-    posts: posts
+app.get('/api/posts', (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched succesfully!',
+      posts: documents
+    });
   });
+  
 
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { HttpClient} from '@angular/common/http';
 
 import { Post } from './post.model';
@@ -12,13 +13,25 @@ export class PostsService {
 
    constructor(private http: HttpClient) {}
 
-   getPosts(){
-     this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-     .subscribe((postData) => {
-       this.posts = postData.posts;
-       this.postsUpdated.next([...this.posts]);
-     });
-   }
+   getPosts() {
+    this.http
+      .get<{ message: string; posts: any }>(
+        "http://localhost:3000/api/posts"
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 
    getPostUpdateListener() {
      return this.postsUpdated.asObservable();
