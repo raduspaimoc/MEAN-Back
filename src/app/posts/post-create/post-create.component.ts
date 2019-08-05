9c8f102fa35720959c135ecc7e1e250cf7a1fc71
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-05 20:33:18 +0200

    File mime type added

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { PostsService} from '../posts.service'
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
+
 
 
 @Component({
@@ -29,7 +31,7 @@ export class PostCreatComponent implements OnInit{
         validators: [Validators.required, Validators.minLength(3)]
       }),
       'content': new FormControl(null, {validators: [Validators.required]}),
-      'image': new FormControl(null, {validators: [Validators.required]})
+      'image': new FormControl(null, {validators: [Validators.required], asyncValidators: mimeType})
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
