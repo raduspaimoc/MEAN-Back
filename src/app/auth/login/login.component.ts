f557a7c9a8abc3a0b44e5003f0c1497efb295d05
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-06 12:26:38 +0200

    Login forms control added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
  @Component({
      templateUrl: './login.component.html',
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 export class LoginComponent {
     isLoading = false;
 
+    onLogin(form: NgForm) {
+        console.log(form.value);
+    }
 }  
