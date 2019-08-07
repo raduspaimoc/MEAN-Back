46253fd6cd9918a257b97faafb425e7d3ce89328
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-07 21:39:41 +0200

    Frontend interceptor added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {
   MatButtonModule,
   MatCheckboxModule,
@@ -24,6 +24,8 @@ import { PostsService } from './posts/posts.service';
 import { AppRoutingModule } from './app-routing.modue';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
+
 
 @NgModule({
   declarations: [
@@ -52,7 +54,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     MatPaginatorModule,
     MatToolbarModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
