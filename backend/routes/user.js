54576384f816ae9ef88d8bbd31b0c2a1578bfccb
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-07 20:54:23 +0200

    Login bug fixed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -29,38 +29,36 @@ router.post("/signup", (req, res, next) => {
 
 router.post("/login", (req, res, next) => {
     let fetchedUser;
-    User.findOne({ email: req.email })
-        .then(user => {        
-            if(!user){
-                return res.status(401).json({
-                    message: "Auth failed"
-                });
-            }
-            fetchedUser = user;
-            return bcrypt.compare(req.body.password, user.password);
-        })
-        .then(result => {
-            if(!result){
-                return res.status(401).json({
-                    message: "Auth failed!"
-                });
-            }
-
-            const token = jwt.sign(
-                { email: fetchedUser.email, userId: fetchedUser._id }, 
-                'secret_token_sux_large',
-                { expiresIn: "1h" }
-            );
-
-            res.status(200).json({
-                token: token
-            });
-        })
-        .catch(err => {
-            return res.status(401).json({
-                message: "Auth failed! Error:" + err
-            })
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+      })
+      .then(result => {
+        if (!result) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        const token = jwt.sign(
+          { email: fetchedUser.email, userId: fetchedUser._id },
+          "secret_this_should_be_longer",
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({
+          token: token
         });
-});
+      })
+      .catch(err => {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      });
+  });
 
 module.exports = router;
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
- @Component({
-     templateUrl: './login.component.html',
-     styleUrls: ['./login.component.css']
- })
+import { AuthService } from "../auth.service";
 
+@Component({
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
 export class LoginComponent {
-    isLoading = false;
+  isLoading = false;
 
-    constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) {}
 
-    onLogin(form: NgForm) {
-        if(form.valid){
-            return;
-        }
-        this.authService.login(form.value.email, form.value.password);
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return;
     }
-}  
+    this.authService.login(form.value.email, form.value.password);
+  }
+}
