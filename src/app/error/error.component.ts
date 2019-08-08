d1737fb84965b722009c83edb0e4c1d73acf6693
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-08 19:27:40 +0200

    Error dialog improved

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
     templateUrl: './error.component.html'
 })
 export class ErrorComponent {
     message = 'An error occurred';
+
+    constructor(@Inject(MAT_DIALOG_DATA) public data: {message: string}) {}
 }
