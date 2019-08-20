285fd607d96203b9daae8afa8f7ae1f8b79ccb03
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-20 13:35:08 +0200

    View Encapsulation

--- ANGULAR-2/1.1 cmp-databinding-start.zip/cmp-databinding-start/src/app/app.component.ts
+++ ANGULAR-2/1.1 cmp-databinding-start.zip/cmp-databinding-start/src/app/app.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {  
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];  
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(bluepintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluepintData.serverName,
+      content: bluepintData.serverContent
+    });
+  }
 }
