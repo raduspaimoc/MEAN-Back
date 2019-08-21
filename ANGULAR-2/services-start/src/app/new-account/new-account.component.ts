fe300d63e3cb0099c814f9aba3d3b1fc560c641c
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-21 19:22:52 +0200

    Hierarchichal service injected

--- ANGULAR-2/services-start/src/app/new-account/new-account.component.ts
+++ ANGULAR-2/services-start/src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
@@ -10,13 +11,11 @@ import { LoggingService } from '../logging.service';
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService){}
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountsService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
