d17798e03a53f99cce1656f7d1df44c72fe90faf
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-22 12:14:36 +0200

    10. Course projecte service dependency injection done

--- ANGULAR-2/my-first-app/src/app/recipes/recipe.service.ts
+++ ANGULAR-2/my-first-app/src/app/recipes/recipe.service.ts
@@ -1,15 +1,38 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService{
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'A Test Recipe', 
+            'This is simply a test', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20),
+            ]),
+        new Recipe(
+            'Another Test Recipe', 
+            'This is simply a test', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Beef', 40),
+                new Ingredient('Tomatoes', 40)
+            ])
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     recipeSelected = new EventEmitter<Recipe>();
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 }
