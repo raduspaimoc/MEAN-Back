f2eee1f4787343a1fb2c31d83a3abe9f08812bb8
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-26 12:52:40 +0200

    App routing module created

--- ANGULAR-2/routing-start/src/app/app.module.ts
+++ ANGULAR-2/routing-start/src/app/app.module.ts
@@ -11,20 +11,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  { path: '**', redirectTo: '/not-found'},   
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent }, 
-  { path: 'users/:id/:name', component: UsersComponent }, 
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id', component: ServerComponent }, 
-  { path: 'servers/:id/edit', component: EditServerComponent },
-  { path: 'not-found', component: PageNotFoundComponent },  
-
-];
 
 @NgModule({
   declarations: [
@@ -39,8 +28,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,  
+    AppRoutingModule 
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
