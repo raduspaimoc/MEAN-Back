f9b56bacbeee89eaeb651b46443b03278ebd0c39
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-27 16:59:58 +0200

    [WIP] observables

--- ANGULAR-2/obs-01-start/src/app/home/home.component.ts
+++ ANGULAR-2/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -22,12 +23,29 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {      
         observer.next(count);
+        if(count == 2){
+          observer.complete();
+        }
+        if(count > 3){
+          observer.error(new Error('Count is greater than 3'));
+          
+        }
         count++;
       }, 1000);
     });
+  
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed lokko');
     });
   }
 
