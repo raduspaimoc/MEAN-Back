51bfb5c5967921eb1f8b32f246849fff84a520d0
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-27 20:56:36 +0200

    Observables done

--- ANGULAR-2/obs-01-start/src/app/app.component.ts
+++ ANGULAR-2/obs-01-start/src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   userActivated = false;
+  private activatedSub: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(didActivate => {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
       this.userActivated  = didActivate;
     });
+  }
+
+  ngOnDestroy(){
+    this.activatedSub.unsubscribe();
   }
 }
