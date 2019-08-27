9652ce12ca3a62606dababaa15fef44e17c6965f
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-27 12:55:27 +0200

    Course project routing done

--- ANGULAR-2/my-first-app/src/app/app.module.ts
+++ ANGULAR-2/my-first-app/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { DropdownDriective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDriective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
