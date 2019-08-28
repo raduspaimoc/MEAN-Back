abec1a8535df0314287d09377dd56082b0b377f7
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-28 18:48:24 +0200

    [Course Project] Recipe Edit form created

--- ANGULAR-2/my-first-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ANGULAR-2/my-first-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -47,6 +47,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     //this.slService.addIngredient(newIngredient);
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete(){
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
   ngOnDestroy(){
     this.subscription.unsubscribe();
   }
