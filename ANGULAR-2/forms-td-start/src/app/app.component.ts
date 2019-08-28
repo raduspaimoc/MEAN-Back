c169c842c233e009992f0ee679d3bd395435d598
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-28 12:39:17 +0200

    Using form data & reseting forms

--- ANGULAR-2/forms-td-start/src/app/app.component.ts
+++ ANGULAR-2/forms-td-start/src/app/app.component.ts
@@ -12,9 +12,33 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+
   }
 
   // onSubmit(form: NgForm){
@@ -22,6 +46,14 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
+
+    this.signupForm.reset();
+
   }
 }
