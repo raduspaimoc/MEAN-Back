71c1908abe908e3cb4b1ba098d275551079d2e72
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-28 17:22:34 +0200

    Reactive Forms assignment solution

--- ANGULAR-2/forms-reactive-start/src/app/app.component.ts
+++ ANGULAR-2/forms-reactive-start/src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {  
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit { 
+  
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, 
+        [Validators.required, CustomValidators.invalidProjectName.bind(this)], 
+        CustomValidators.asyncInvalidProjectName),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical')
+    });    
+  }
+
+  onSaveProject() {
+    console.log(this.projectForm.value);
+  }
+
+  /*genders = ['male', 'female'];
   signupForm: FormGroup;
   forbiddenUsernames = ['Chris', 'Anna'];
 
@@ -69,5 +87,5 @@ export class AppComponent implements OnInit {
       }, 1500);
     });
     return promise;
-  }
+  }*/
 }
