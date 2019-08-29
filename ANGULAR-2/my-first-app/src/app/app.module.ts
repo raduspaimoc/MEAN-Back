915246dff78a2bc1310d588d403ea8a03592b5f3
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-29 13:33:02 +0200

    [Course Project] Forms done

--- ANGULAR-2/my-first-app/src/app/app.module.ts
+++ ANGULAR-2/my-first-app/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
